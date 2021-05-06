cf1f4568b64a26a3ee2910c88e035c457288070b
chefk5 <kncharkie@gmail.com>
2021-05-06 14:16:26 +0300

    First commit

---
+++ api/models/todoModel.js
@@ -0,0 +1,25 @@
+'use strict';
+// Import mongoose
+    const mongoose = require("mongoose");
+
+// Declare schema and assign Schema class
+    const Schema = mongoose.Schema;
+
+// Create Schema Instance and add schema propertise
+    const TodoSchema = new Schema({
+        type: {
+            type:String,
+            required:true
+        },
+        amount:{
+            type:Number,
+            required:true
+        },
+        createdOn: {
+            type:Date,
+            default:Date.now
+        }
+    });
+
+// create and export model
+module.exports = mongoose.model("todoModel", TodoSchema);
---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// require express and bodyParser
+const  express = require("express");
+const  bodyParser = require("body-parser");
+require('dotenv').config()
+
+require("./config/db"); 
+
+
+// create express app
+const app = express();
+
+// define port to run express app
+const  port = process.env.PORT || 3000;
+
+// use bodyParser middleware on express app
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+// Add endpoint
+app.get('/', (req, res) => {
+res.send("Hello World");
+});
+var routes = require('./api/routes/todoRoutes'); //importing route
+routes(app);
+// Listen to server
+app.listen(port, () => {
+console.log(`Server running at http://localhost:${port}`);
+});
